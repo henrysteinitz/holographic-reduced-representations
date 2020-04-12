be465dfaa34d9f9895aac5faa793893bb6421208
Henry Steinitz <henrysteinitz@google.com>
2020-04-12 16:16:23 -0400

    Add two tests and a more complete interface

--- hrr.py
+++ hrr.py
@@ -16,8 +16,13 @@ class HRR:
 
 
 	@property
+	def square_modulus(self):
+		return self.real ** 2 + self.im ** 2
+
+
+	@property
 	def modulus(self):
-		return torch.sqrt(self.real ** 2 + self.im ** 2)
+		return torch.sqrt(self.square_modulus)
 
 
 	@property
@@ -25,9 +30,14 @@ class HRR:
 		# TODO: Handle zeros.
 		return torch.atan(self.im / self.real)
 
+
 	@property	
 	def conj(self):
 		return HRR(real=self.real, im=-1 * self.im)
+
+	@property
+	def inv(self):
+		return (self.conj / self.square_modulus) 
 
 
 	def bind(self, x):
@@ -45,13 +55,17 @@ class HRR:
 
 
 	def set(self, k, v):
-		return self.store(self.bind(k, v))
+		return self.store(k.bind(v))
 
 
 	def get(self, k):
-		# TODO: Compare conjugate retrieval to inverse
-		# retrieval
-		return k.conj * self
+		return k.inv * self
+
+
+	def get_phase(self, k):
+		# If we're only interested in recovering the phase,
+		# we can use the conjugate instead of the inverse.
+		return (k.conj * self).phase
 
 
 	def __add__(self, x):
@@ -60,5 +74,11 @@ class HRR:
 
 	def __mul__(self, x):
 		return self.bind(x)
+
+
+	def __truediv__(self, x):
+		# Pointwise divide by tensor
+		# TODO: Also support / HRR
+		return HRR(self.real / x, self.im / x)
 	
 	
--- test.py
+++ test.py
@@ -1,3 +1,66 @@
+from hrr import HRR
 import torch
+import unittest
 
-# TODO: test
+
+class HRRTests(unittest.TestCase):
+	def test_single_pair(self):
+		size = (40, 30)
+		real = .01 * torch.rand(size)
+		im = .01 * torch.rand(size)
+		mem = HRR(real, im)
+
+		k = HRR(2*torch.rand(size), 3*torch.rand(size))
+		v = HRR(5*torch.ones(size), 2*torch.ones(size))
+		mem = mem.set(k, v)
+
+		restored = mem.get(k)
+		torch.testing.assert_allclose(
+			restored.real, 
+			v.real, 
+			rtol=0, 
+			atol=.1
+		)
+		torch.testing.assert_allclose(
+			restored.im, 
+			v.im, 
+			rtol=0, 
+			atol=.1
+		)
+		restored_phase = mem.get_phase(k)
+		torch.testing.assert_allclose(
+			restored_phase, 
+			v.phase, 
+			rtol=0, 
+			atol=.1
+		)
+
+	def test_multiple_pairs(self):
+		size = (40, 30)
+		real = .01 * torch.rand(size)
+		im = .01 * torch.rand(size)
+		mem = HRR(real, im)
+
+		k = HRR(torch.rand(size), torch.rand(size))
+		v = HRR(5*torch.ones(size), 2*torch.ones(size))
+		mem = mem.set(k, v)
+
+		k = HRR(torch.rand(size), torch.rand(size))
+		v = HRR(5*torch.ones(size), 2*torch.ones(size))
+		mem = mem.set(k, v)
+
+		k = HRR(torch.rand(size), torch.rand(size))
+		v = HRR(5*torch.ones(size), 2*torch.ones(size))
+		mem = mem.set(k, v)
+
+		restored_phase = mem.get_phase(k)
+		torch.testing.assert_allclose(
+			restored_phase, 
+			v.phase, 
+			rtol=0, 
+			atol=.1
+		)
+		# TODO: Do we need duplicates to fix this test?
+
+if __name__ == '__main__':
+    unittest.main()
